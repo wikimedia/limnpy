d561a40418dd41875d9b82b26f518daf2dcd34fb
Evan Rosen <erosen@wikimedia.org>
2013-01-07 14:10:26 -0800

    updates help info and adds some support for dealing with default date parsing and default datecol=0

--- limnpy/limnify.py
+++ limnpy/limnify.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import os.path
 import datetime
+import dateutil.parser
 import sys
 
 sys.path.insert(0, os.path.abspath('..'))
@@ -10,30 +11,40 @@ sys.path.insert(0, os.path.abspath('..'))
 import limnpy
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('csv', help='name of file to be transformed')
-    parser.add_argument('--delim', default='\t', help='delim to use for input csv')
-    parser.add_argument('--header', default=None, nargs='+', help='')
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('data', help='name of file to be limnified')
+    parser.add_argument('--delim', default=',', help='delim to use for input file')
+    parser.add_argument('--header', default=None, nargs='+', help='this is a space separated list of names to use as the header row'
+                'If your data doesn\'t already have a header row you will need to pass in a list of names to use, otherwise it'
+                'will assume the first row is a header and then produce confusing data sources.  Remember, these names will be'
+                'displayed in the graph editing interface')
+    parser.add_argument('--datecol', type=int, default=0, help='the date column name or index--required if it is different from `date`')
+    parser.add_argument('--datefmt', help='format to use with datetime.strptime, default uses dateutil.parser.parse')
 
-    parser.add_argument('--pivot', default=False, action='store_true', help='whether to try and automatically pivot the data')
-    parser.add_argument('--datecol', type=int, default=0, help='the date column')
-    parser.add_argument('--datefmt', help='format to use with datetime.strptime')
-    parser.add_argument('--metriccol', default=1, help='the column to use for creating distinct metrics')
-    parser.add_argument('--valcol', default=2, help='the column to use for the actual data to be plotted')
+    parser.add_argument('--pivot', default=False, action='store_true', help='whether to try and pivot the data '
+                '(only supports sum aggregation for now)')
+    parser.add_argument('--metriccol', default=1, help='the column name or index to use for creating the column (metric) names when pivoting')
+    parser.add_argument('--valcol', default=2, help='the column in which to find the actual data to be plotted when pivoting')
 
-    parser.add_argument('--basedir', default='.', help='directory in which to place the datasources, datafiles and graphs directories')
-    parser.add_argument('--name', nargs='+', type=' '.join, help='name of datasource')
-    parser.add_argument('--id', help='datasource id')
-    parser.add_argument('--write_graph', default=False, help='whether to write the graph')
+    parser.add_argument('--basedir', default='.', help='directory in which to place the output datasources, datafiles and graphs directories')
+    parser.add_argument('--name', nargs='+', type=' '.join, help='name of datasource which will be displayed in the UI')
+    parser.add_argument('--id', help='the slug / id used to uniquely identify the datasource within a limn installation')
+    parser.add_argument('--write_graph', default=False, help='whether to write a graph file containing all columns from the datasource')
 
     args = parser.parse_args()
 
-    date_parser = lambda s : datetime.datetime.strptime(s, args.datefmt)
+    if args.datefmt:
+        date_parser = lambda s : datetime.datetime.strptime(s, args.datefmt)
+    else:
+        date_parser = dateutil.parser.parse
 
     if not args.pivot:
-        df = pd.read_table(args.csv, sep=args.delim, parse_dates=[args.datecol], date_parser=date_parser, header=args.header)
+        df = pd.read_table(args.data, sep=args.delim, parse_dates=[args.datecol], date_parser=date_parser, header=args.header)
+        if isinstance(args.datecol, int):
+            args.datecol = df.columns[args.datecol] 
+    
     else:
-        df_long = pd.read_table(args.csv, sep=args.delim, parse_dates=[args.datecol], date_parser=date_parser, header=args.header)
+        df_long = pd.read_table(args.data, sep=args.delim, parse_dates=[args.datecol], date_parser=date_parser, header=args.header)
         print df_long
 
         if isinstance(args.datecol, int):
@@ -49,10 +60,10 @@ def main():
     print df
 
     if args.name is None:
-        args.name = os.path.splitext(args.csv)[0]
+        args.name = os.path.splitext(args.data)[0]
     if args.id is None:
-        args.id = os.path.splitext(args.csv)[0]
-    ds = limnpy.DataSource(args.id, args.name, df)
+        args.id = os.path.splitext(args.data)[0]
+    ds = limnpy.DataSource(args.id, args.name, df, date_key=args.datecol)
     ds.write(args.basedir)
 
     if args.write_graph:
